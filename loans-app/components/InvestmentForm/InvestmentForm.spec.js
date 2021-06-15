38a1836e2bb13ae06fcbee462ad666ed889bf4ea
Abdoul Sy <dreescan@gmail.com>
2021-06-15 16:38:05 +0100

    Contructing the investment form

--- loans-app/components/InvestmentForm/InvestmentForm.spec.js
+++ loans-app/components/InvestmentForm/InvestmentForm.spec.js
@@ -1,7 +1,34 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react'
+import { render, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import InvestmentForm from "./"
+
 describe("Given <InvestmentForm />", () => {
     describe("And `_LoanData` and `_TotalInvestmentAvailable`", () => {
+        const _LoanData = {
+            "id": "1",
+            "title": "Voluptate et sed tempora qui quisquam.",
+            "tranche": "A",
+            "available": "11,959",
+            "annualised_return": "8.60",
+            "term_remaining": "864000",
+            "ltv": "48.80",
+            "amount": "85,754"
+        }
+        const _TotalInvestmentAvailable = 1000000;
+
         describe("When rendered by a User", () => {
-            it("should show an <Input />")
+            beforeEach(async () => {
+                render(<InvestmentForm loadData={_LoanData} totalAvailable={_TotalInvestmentAvailable} />)
+              
+                await waitFor(() => screen.getByRole('form'))
+            })
+            it("should show an <Input />", () => {
+                expect(screen.getByRole('input')).toBeInTheDocument()
+            })
             it.todo("should show the correct `_LoanData`")
             it.todo("should show a <CloseButton />")
     
