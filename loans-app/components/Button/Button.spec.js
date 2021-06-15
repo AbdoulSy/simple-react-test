4ca94e744326da42a16bba78be32b1e08541925a
Abdoul Sy <dreescan@gmail.com>
2021-06-15 15:57:00 +0100

    Basic tests for the Button

--- loans-app/components/Button/Button.spec.js
+++ loans-app/components/Button/Button.spec.js
@@ -1,9 +1,23 @@
+/**
+ * @jest-environment jsdom
+ */
+import React from 'react'
+import { render, waitFor, screen } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import {Button} from './'
+
 describe("Given <Button />", () => {
     describe("And with `label` and `onClick`", () => {
+        const label = "Invest !"
+        const onClick = jest.fn()
+    
         describe("When rendered by a User", () => {
-            it.todo("should show the expected `label`")
-            describe("And a user clicks on <Button />", () => {
-                it.todo("should call the `onClick` function")
+            it("should show the expected `label`", async () => {
+                render(<Button onClick={onClick} label={label} />)
+              
+                await waitFor(() => screen.getByRole('button'))
+              
+                expect(screen.getByRole('button')).toHaveTextContent(label)
             })
         })
     })
